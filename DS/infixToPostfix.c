f1d6d843fb58ddad5fa55acaa6616a0ac8a3030f
Omkar1027 <omkadam532@gmail.com>
2024-08-07 22:03:36 +0530

    infix to postfix

--- DS/infixToPostfix.c
+++ DS/infixToPostfix.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
+#include <string.h>
 
 int stackIndex=-1;
 
-void preference()
+int preference(char c){
+    if(c=='^') return 3;
+    else if(c=='*'||c=='/') return 2;
+    else if(c=='+'||c=='-') return 1;
+    else return -1;
+}
 
-void print_stack(char arr[], int array_index){
-     for(int i=0;i<array_index;i++){
-        printf("\narr[%d]=%c ",i,arr[i]);
+void print_stack(char arr[], int index){
+     for(int i=0;i<index;i++){
+        printf("%c",arr[i]);
     }
 }
 
@@ -18,19 +24,61 @@ int pop(char stack[]){
     return stack[stackIndex--];
 }
 
-void iToP(int infix[]){
-    int postfix[100], stack[100];
-    for(int i=0; i<strlen(infix); i++){
-        int ichar= infix[i];
-        if(ichar>='0'&& ichar<='9'){
-            push(stack,ichar);
+void iToP(char infix[]){
+    char postfix[100], stack[100];
+    int postfixInd=-1, checkP, checkPI, len;
+    push(stack,'(');
+    len=strlen(infix);
+    len++;
+    infix[len]=')';
+
+    for(int i=0; i<len; i++){
+        char ichar= infix[i];
+        if((ichar>='0'&& ichar<='9')||(ichar>='a'&& ichar<='z') || (ichar>='A'&& ichar<='Z')){
+            postfix[++postfixInd]=ichar;        
+            printf("Enter if 1\n");
         }
+        else if(ichar=='('){
+            push(stack,ichar);
+            printf("Enter else if 1\n");
+        }
+        else if(ichar==')'){
+            while(stackIndex>-1 && stack[stackIndex]!='('){
+                postfix[postfixInd++]=stack[stackIndex--];
+            }
+            stackIndex--;
+            printf("Enter else if 2\n");
+        }
+        else {
+            checkP = preference(ichar);
+            checkPI =preference(stack[stackIndex-1]) ;
+            if(checkPI!=-1){
+                if(checkP>checkPI){
+                    while(stack[stackIndex]!='('){
+                        postfix[postfixInd++]=stack[stackIndex--];
+                    }
+                }
+                else{
+                    push(stack,ichar);
+                }
+            }
+            
+        }
+        
     }
-    
-}
+    if(stackIndex==-1){
+        printf("Postfix expression: ");
+        print_stack(postfix,postfixInd);
+    }
+    else{
+        printf("Invalid infix expression :`)");
+        print_stack(postfix,postfixInd);
+    }
+}   
+
 
 int main(){
-    int infix[100];
+    char infix[100];
 
     printf("Enter the infix expression: ");
     scanf("%s", &infix);
